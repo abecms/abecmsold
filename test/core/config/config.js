ee23d2bd0f59bd138dde57799de4a55631beda84
gregorybesson <gregory.besson@adfab.fr>
2017-01-15 07:54:50 -0500

    refactoring:cleaning this version

--- test/core/config/config.js
+++ test/core/config/config.js
@@ -5,11 +5,8 @@ var config = require('../../../src/cli').config
 config.set({root: path.join(process.cwd(),'test','fixtures')})
 
 describe('Config', function() {
-  /**
-   * getAbeImport
-   * 
-   */
   it('configuration file', function() {
+    console.log(config)
     chai.assert.equal(config.localeFolder, 'locale', 'Error on config file')
     chai.assert.equal(config.root.substr(config.root.lastIndexOf(path.sep) + 1), 'fixtures', 'Error on config file')
     chai.assert.equal(config.upload.image, 'unitimage', 'Error on config file')
